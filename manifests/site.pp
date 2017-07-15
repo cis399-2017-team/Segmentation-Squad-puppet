b52930385c3dfe759ae6d58d7041b980dd68ec0e
Jeremy Unck <jeremy@Jeremys-MacBook-Pro.local>
2017-07-15 09:59:56 -0700

    cron job for puppet master

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
 node ip-10-0-5-119 {
+	cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/5",
+	}
 }
 
 node ip-10-0-5-93 {
