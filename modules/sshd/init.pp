648204e0f3da8c9e5d28eabb85d8344a80ae2baf
Syd <slynch2@uoregon.edu>
2017-07-16 16:34:05 -0700

    module update

--- modules/sshd/init.pp
+++ modules/sshd/init.pp
@@ -1,20 +1,39 @@
 class sshd {
         package {
-                "openssh-server": ensure => installed;
+                "openssh-server": 
+                	ensure => installed;
         }
 
         service {
-                "openssh-server": enable => true,
-                ensure => running;
+                "openssh-server": 
+                	enable => true,
+                	ensure => running,
+                	hasstatus => true,
+                	hasrestart => true,
+                	require => [ Package["openssh-server"],
+                				 File["/etc/ssh/sshd_config"] ],
+                	subscribe => File["/etc/ssh/sshd_config"];
         }
         
+  		file {
+  				"/etc/ssh/sshd_config": 
+  					source => ["puppet:///modules/sshd/files/sshd_config"],
+  					notify => Service['sshd'],
+  					mode => "644",
+  					owner => "root",
+  					group => "root",
+  					require => Package['openssh-server'],
+  		}      
+
         ssh_authorized_key {
         	"slynch": user => "ubuntu",
+        	type => 'rsa',
         	key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCsfkdPqW0Vm6T5PcQYaBid7eXpd3wK54x9Qq50tDYenN1CfyTIM6mEJYYxtIpKwV5oqMiJLJ4PyfpuWCpl1DjIvFhnFtpv8p50yc3ZNNQvNbGhpSMp2G6RmTnmsSmTLk3FEThd4OIfhWCCawd3WaJbSQqyjg5ymr/VYj+rYauC0nT19VHrgqw2lr/HVItXTt0N6JTu01WLTvs75n988OLLkSeJL7pycFfo8ve+pK8phANsUNos42dug+8zQwHo4tqsWNh+o1tMCrOcoQjgOSIXyS9EuAkg8sE37cAoYZ3RT3zZoz+c1qtRg6Bjp/u7Hac+ac28cYzQt2/ja0oQRdil";
         }
 
         ssh_authorized_key {
         	"junck": user => "ubuntu",
+        	type => 'rsa',
         	key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCjfk9PZxnJkmtLqICp/Lj7GrAiqnxAsTKcUV7ATJFY9DwjIJdpXUsD+Yzqc/C32tCdXvmU54qmORYzYb9tmzHVfWEQeTg3mQzBwDmOiwU9MksaQwoGZNMuX9tkFagtAKIsCbDs7te6NJB7k5RwsZwXF70RtxXOgOGx76fizZ+HI/b4Ft0YYb9sDwvBbww7dj5lJQWOMjxl077BUNTMAeEFMKggRBFyjTHGOp8yKhdcEwbZvtIx3Gt46mA3YmVkJHdXiClpdaz/gAACPRlXRiKm9Q494e1yJZUZ8FleQDh2VX9ODiniT7usv1lmaxBH/Ugc7Y99jSWF4Mkoi2NTWbqz";
         }
 
