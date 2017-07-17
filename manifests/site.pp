5724c5dbdd0eb390d30d9aee387787d3fced6d7d
Syd <slynch2@uoregon.edu>
2017-07-16 18:43:36 -0700

    updated site and init

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,6 @@
 node ip-10-0-5-119 {
+	include sshd
+
 	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",
 	    user    => root,
@@ -7,7 +9,9 @@ node ip-10-0-5-119 {
 }
 
 node ip-10-0-5-93 {
+	include sshd
 }
 
 node ip-10-0-5-134 {
+	include sshd
 }
