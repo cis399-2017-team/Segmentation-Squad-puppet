630cbd25f4ab39ff7e1ee12ae44ce7048d6a99b8
Jeremy Unck <root@ip-10-0-5-134.us-west-2.compute.internal>
2017-08-17 22:50:54 +0000

    changed localhost mysql password

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,6 @@
 node ip-10-0-5-134 {
 	include sshd
+	
 
 	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",
@@ -10,17 +11,38 @@ node ip-10-0-5-134 {
 	group { '399':
   		ensure => 'present',
   		gid    => '1001',
-	}                                                                         
+	}
+
+	class { '::mysql::server':
+		root_password => 'root',
+		override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+	}                                                                       
+
 }
 
 node ip-10-0-5-192 {
 	include apache
 	include sshd
 	include users
+	class { '::mysql::server':
+                root_password => 'segsquad',
+                override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+        }
+
+
 }
 
 node ip-10-0-5-73 {
 	include apache
 	include sshd
 	include users
+	class { 'mysql::client':}
+	class { '::mysql::server':
+                root_password => 'segsquad',
+                override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+        }
+
+
 }
+
+
